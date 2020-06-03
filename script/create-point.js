c496e01eca8b24489e67cd09bbb289ade6e86c60
raphael <rafadastan02@live.com>
2020-06-02 22:00:47 -0300

    identando o código

--- script/create-point.js
+++ script/create-point.js
@@ -1,37 +1,37 @@
-function populateUFs(){    
+function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
 
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
         .then(res => res.json())
-        .then( states => {
-            for(const state of states){
-            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        .then(states => {
+            for (const state of states) {
+                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
             }
-        })                    
+        })
 }
 
 populateUFs()
 
-function getCities(event){
+function getCities(event) {
     const citySelect = document.querySelector("[name=city]")
     const stateInput = document.querySelector("[name=state]")
-    
+
     const ufValue = event.target.value
 
     const indexOfSelectedState = event.target.selectedIndex
     stateInput.value = event.target.options[indexOfSelectedState].text
 
-    const url = `http://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios` 
+    const url = `http://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
     fetch(url)
         .then(res => res.json())
-        .then( cities => {
-            for(const city of cities){
+        .then(cities => {
+            for (const city of cities) {
                 citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
             }
 
             citySelect.disabled = false
-        })  
+        })
 }
 
 document
